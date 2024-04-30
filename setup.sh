f83c98beed4fd03ac9d28c16aa851008b2805469
Frédéric Arroyo <71824723+frederic-arr@users.noreply.github.com>
2024-04-30 11:22:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/nixos-main/disko.nix
 mkdir -p /mnt/etc/nixos/
-cp -a /tmp/nixos-config/ /mnt/etc/nixos/
+cp -a /tmp/nixos-main/ /mnt/etc/nixos/
 nixos-generate-config --no-filesystems --show-hardware-config > /mnt/etc/nixos/hardware-configuration.nix

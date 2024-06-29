209b950b47df54c567363c8859da63805300225a
User <user@example.com>
2024-06-29 17:39:47 +0200

    locale

--- hosts/common/core/locale.nix
+++ hosts/common/core/locale.nix
@@ -7,11 +7,11 @@
     LC_IDENTIFICATION = "de_CH.UTF-8";
     LC_MEASUREMENT = "de_CH.UTF-8";
     LC_MONETARY = "de_CH.UTF-8";
-    LC_NAME = "de_CH.UTF-8";
+    LC_NAME = "en_DK.UTF-8";
     LC_NUMERIC = "de_CH.UTF-8";
     LC_PAPER = "de_CH.UTF-8";
     LC_TELEPHONE = "de_CH.UTF-8";
-    LC_TIME = "de_CH.UTF-8";
+    LC_TIME = "en_DK.UTF-8";
   };
   services.xserver.xkb = {
     layout = "ch";
